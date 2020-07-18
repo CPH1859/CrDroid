58795cdbf0140dc0e27813a2c0ec78270dd0eda9
devil-black-786 <60068093+devil-black-786@users.noreply.github.com>
2020-07-18 16:40:46 +0530

    Update and rename aosp_CPH1859.mk to bootleg_CPH1859.mk

--- bootleg_CPH1859.mk
+++ bootleg_CPH1859.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Inherit from oppo CPH1861
 $(call inherit-product, device/oppo/CPH1859/device.mk)
@@ -35,7 +35,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_CPH1859
+PRODUCT_NAME := bootleg_CPH1859
 PRODUCT_DEVICE := CPH1859
 PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BRAND := OPPO
